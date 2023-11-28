f02359352c5cd863c1f925b05a35fc95126418ba
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-11-27 20:54:12 -0400

    Validaciones en formularios (2da parte)

---
+++ PasosIniciales/Controllers/ValidacionController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasosIniciales.Models;
+
+namespace PasosIniciales.Controllers
+{
+    [Route("validacion")]
+    public class ValidacionController : Controller
+    {
+        [Route("index")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("Index", new Usuario());
+        }
+        [Route("guardar")]
+        [HttpPost]
+        public IActionResult Guardar(Usuario usuario)
+        {
+            if(ModelState.IsValid)
+            {
+                ViewBag.usuario = usuario;
+                return View("Registrado");
+            }else
+                return View("Index", new Usuario());//No es necesario pasarle el modelo
+        }
+    }
+}
--- PasosIniciales/Models/Ususario.cs
+++ PasosIniciales/Models/Ususario.cs
@@ -5,16 +5,16 @@ namespace PasosIniciales.Models
     public class Usuario
     {
         [Required]
-        [MinLength(3)]
+        [MinLength(3, ErrorMessage ="El nombre de usuario debe ser de 3 letras o mas")]
         [MaxLength(15)]
         public string Nombre { get; set; }
         [Required]
-        [MinLength(5)]
+        [MinLength(5,ErrorMessage = "Por favor, ingrese un  password con un minimo de 5 caracteres")]
         [MaxLength(20)]
-        [RegularExpression("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{5,20})")]
+        [RegularExpression("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{5,20})", ErrorMessage = "El password debe contener minusculas, mayusculas, numeros y simbolos (@#$%)")]
         public string Password { get; set; }
         [Required]
-        [Range(18,30)]
+        [Range(18,30, ErrorMessage = "La edad debe estar entre 18 y 30")]
         public int Edad {  get; set; }
         [EmailAddress]
         public string Email { get; set; }
