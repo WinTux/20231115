9f837d3fb31114b7cf5c29713fe2cde076b1a5cd
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-11-30 21:30:29 -0400

    ado.net

---
+++ PasosIniciales/Controllers/AdoNetController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace PasosIniciales.Controllers
+{
+    [Route("asp")]
+    public class AdoNetController : Controller
+    {
+        [Route("index")]
+        public IActionResult Index()
+        {
+            string cadenaConexion = "Server=192.168.1.253;Database=Tienda;User=sa;Password=123456ABCxyz";
+            SqlConnection conexion = new SqlConnection(cadenaConexion);// cambiar userid
+            conexion.Open();
+
+            conexion.Close();
+            return View();
+        }
+    }
+}
