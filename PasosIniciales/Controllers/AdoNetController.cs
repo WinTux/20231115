7b37d0a013c93204ba3f67f4e69e731bb41cf482
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-12-01 20:43:24 -0400

    ADO.NET 2

--- PasosIniciales/Controllers/AdoNetController.cs
+++ PasosIniciales/Controllers/AdoNetController.cs
@@ -9,11 +9,28 @@ namespace PasosIniciales.Controllers
         [Route("index")]
         public IActionResult Index()
         {
-            string cadenaConexion = "Server=192.168.1.253;Database=Tienda;User=sa;Password=123456ABCxyz";
-            SqlConnection conexion = new SqlConnection(cadenaConexion);// cambiar userid
-            conexion.Open();
+            string cadenaConexion = "Server=192.168.1.253;DataBase=Tienda;User ID=sa;Password=123456ABCxyz;Trust Server Certificate=true";
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion)) {
+                conexion.Open();
+                using (SqlCommand comando = new SqlCommand("select * from Producto;",conexion)) {
+                    using (var reader = comando.ExecuteReader()) {
+                        while (reader.Read()) {
+                            string id = reader.GetString(0);
+                            string nombre = reader.GetString(1);
+                            string? foto = reader.GetString(3);
+                            ViewBag.id = id;
+                            ViewBag.nombre = nombre;
 
-            conexion.Close();
+                            ViewBag.foto = foto;
+                        }
+                        
+
+                    }
+                }
+            }
+            
+
+            
             return View();
         }
     }
