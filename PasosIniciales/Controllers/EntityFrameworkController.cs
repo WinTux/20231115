9e1e670cb80dbeaaf0203fb1c4c7523de035411a
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-12-04 21:33:57 -0400

    CRUD con EntityFramework

--- PasosIniciales/Controllers/EntityFrameworkController.cs
+++ PasosIniciales/Controllers/EntityFrameworkController.cs
@@ -1,12 +1,70 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PasosIniciales.Models;
 
 namespace PasosIniciales.Controllers
 {
+    [Route("ef")]
     public class EntityFrameworkController : Controller
     {
+        private TiendaDbContext _context;
+        public EntityFrameworkController(TiendaDbContext context)
+        {
+            _context = context;
+        }
+        [Route("index")]
         public IActionResult Index()
         {
+            ViewBag.productos = _context.Productos.ToList();
             return View();
+        }
+
+        [Route("insertar")]
+        [HttpGet]
+        public IActionResult Insertar()
+        {
+            return View();
+        }
+
+        [Route("insertar")]
+        [HttpPost]
+        public IActionResult Insertar(Producto3 producto)
+        {
+            _context.Productos.Add(producto);
+            _context.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+        [Route("modificar")]
+        [HttpGet]
+        public IActionResult Modificar()
+        {
+            return View();
+        }
+
+        [Route("modificar")]
+        [HttpPost]
+        public IActionResult Modificar(Producto3 producto)
+        {
+            _context.Entry(producto).State = EntityState.Modified;
+            _context.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+        [Route("eliminar")]
+        [HttpGet]
+        public IActionResult Eliminar()
+        {
+            return View();
+        }
+
+        [Route("eliminar")]
+        [HttpPost]
+        public IActionResult Eliminar(string id)
+        {
+            _context.Productos.Remove(_context.Productos.Find(id));
+            _context.SaveChanges();
+            return RedirectToAction("index");
         }
     }
 }
--- PasosIniciales/Models/Producto3.cs
+++ PasosIniciales/Models/Producto3.cs
@@ -1,9 +1,17 @@
-﻿namespace PasosIniciales.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PasosIniciales.Models
 {
+    [Table("Producto")]
     public class Producto3
     {
+        [Key]
         public string Id { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Nombre { get; set; }
+        [Required]
         public Decimal Precio { get; set; }
         public string? Foto { get; set; }
     }
